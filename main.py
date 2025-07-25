0badad8fd55f75f8807abacc04924efffcf6b2c0
vibhaw raj <vibhawraj494@gmail.com>
2025-07-25 13:43:45 +0530

    [test]:anthropic

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+import getpass
+import os
+
+if not os.environ.get("ANTHROPIC_API_KEY"):
+  os.environ["ANTHROPIC_API_KEY"] = getpass.getpass("Enter API key for Anthropic: ")
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("claude-3-5-sonnet-latest", model_provider="anthropic")
